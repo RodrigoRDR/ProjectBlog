793a02feb278e7b7c650a6b23dc116a4f79a5e21
Rodrigo Reis <rodrigoreis432@gmail.com>
2024-03-28 17:56:54 -0300

    ProjectBlog

---
+++ djangoapp/blog/views.py
@@ -0,0 +1,42 @@
+from django.core.paginator import Paginator
+from django.shortcuts import render
+from blog.models import Post
+
+
+PER_PAGE = 9
+
+
+def index(request):
+    posts = Post.objects.get_published()
+
+    paginator = Paginator(posts, PER_PAGE)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
+    return render(
+        request,
+        'blog/pages/index.html',
+        {
+            'page_obj': page_obj,
+        }
+    )
+
+
+def page(request):
+    return render(
+        request,
+        'blog/pages/page.html',
+        {
+            # 'page_obj': page_obj,
+        }
+    )
+
+
+def post(request):
+    return render(
+        request,
+        'blog/pages/post.html',
+        {
+            # 'page_obj': page_obj,
+        }
+    )
